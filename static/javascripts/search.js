90e63a4448f7eef929b34aa99ca64b77dadca973
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-11-27 20:04:24 -0800

    optimaize search js color transitions

--- static/javascripts/search.js
+++ static/javascripts/search.js
@@ -1,19 +1,19 @@
 $("label input").on("click",function(){
     $('label').css('background', 'transparent')
     $('label').css('color', 'black')
+    $(this).parent().css('color', 'white')
     if ($(this).val()=='Playstation-3' || $(this).val()=='Playstation-4'){
       $(this).parent().css('background', 'linear-gradient(to right, rgb(1,72,153), rgb(1, 132, 212)')
     }
-    if ($(this).val()=='Xbox-One' || $(this).val()=='Xbox-360'){
+    else if ($(this).val()=='Xbox-One' || $(this).val()=='Xbox-360'){
       $(this).parent().css('background', 'linear-gradient(to right, green, rgb(33, 188, 70)')
 
     }
-    if ($(this).val()=='Wii' || $(this).val()=='Wii-U'){
+    else if ($(this).val()=='Wii' || $(this).val()=='Wii-U'){
       $(this).parent().css('background', 'linear-gradient(to right, gray, rgb(180, 180, 180)')
 
     }
-    if ($(this).val()=='Other'){
+    else if ($(this).val()=='Other'){
       $(this).parent().css('background', 'linear-gradient(to right, black, rgb(80, 80, 80)')
     }
-    $(this).parent().css('color', 'white')
 });
